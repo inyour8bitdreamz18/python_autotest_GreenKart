4c5637130fa8f4b2d32b775641461dce381dd513
inyour8bitdreamz18 <WaginElla2019>
2025-10-26 19:49:53 +0300

    Created class Application, helper methods have been moved, added Fixture, replaced Unittest with Pytest

--- user.py
+++ user.py
@@ -4,18 +4,18 @@ class User:
 
     def __init__(self, firstname, lastname, email, telephone, fax, company, address1, address2,
                                      city, zone_id, postcode, country_id, loginname, password):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
+        self.firstname = firstname   # required
+        self.lastname = lastname     # required
+        self.email = email           # required
         self.telephone = telephone
         self.fax = fax
         self.company = company
-        self.address1 = address1
+        self.address1 = address1     # required
         self.address2 = address2
-        self.city = city
-        self.zone_id = zone_id
-        self.postcode = postcode
-        self.country_id = country_id
-        self.loginname = loginname
-        self.password = password
+        self.city = city             # required
+        self.zone_id = zone_id       # required
+        self.postcode = postcode     # required
+        self.country_id = country_id # required
+        self.loginname = loginname   # required
+        self.password = password     # required
 
